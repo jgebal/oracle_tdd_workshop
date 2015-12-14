91836a919708c4298df10e8ba3c88f8ca5601cea
Jacek Gębal <jgebal@gmail.com>
2015-12-14 21:46:49 +0000

    Added additional tests

--- spec/empolyees_api/get_employee_spec.rb
+++ spec/empolyees_api/get_employee_spec.rb
@@ -10,10 +10,16 @@ create or replace package body employees_api as
   function get_employee( p_employee_id integer ) return employees%rowtype is
     v_result employees%rowtype;
   begin
+    if p_employee_id is null then
+      raise_application_error(-20001, 'Null parameter value given');
+    end if;
     select *
       into v_result
       from employees where employee_id = p_employee_id;
     return v_result;
+  exception
+    when no_data_found then
+      raise_application_error(-20000, 'The employee with id = '||p_employee_id||' was not found');
   end;
 end;
 SQL
@@ -26,4 +32,11 @@ describe 'get employee' do
     expect( plsql.employees_api.get_employee(100) ).to eq(expected)
   end
 
+  it 'returns raised if record is not found' do
+    expect{ plsql.employees_api.get_employee(-100) }.to raise_exception(/employee.* was not found/)
+  end
+
+  it 'returns raises if null value given' do
+    expect{ plsql.employees_api.get_employee(NULL) }.to raise_exception(/Null parameter value given/)
+  end
 end
