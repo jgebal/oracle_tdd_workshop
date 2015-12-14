a0ad7c5b7b12aef05a9b29760e6f1a08150900d4
Jacek Gębal <jgebal@gmail.com>
2015-12-14 21:36:52 +0000

    Added first implementation and fixed the test

--- spec/empolyees_api/get_employee_spec.rb
+++ spec/empolyees_api/get_employee_spec.rb
@@ -1,8 +1,29 @@
+plsql.execute <<-SQL
+create or replace package employees_api as
+  function get_employee( p_employee_id integer ) return employees%rowtype;
+end;
+SQL
+
+
+plsql.execute <<-SQL
+create or replace package body employees_api as
+  function get_employee( p_employee_id integer ) return employees%rowtype is
+    v_result employees%rowtype;
+  begin
+    select *
+      into v_result
+      from employees where employee_id = p_employee_id;
+    return v_result;
+  end;
+end;
+SQL
+
+
 describe 'get employee' do
 
   it 'returns a single employee for a given id' do
-    expected = plsql.employees.first('where employee_id = :id', 1)
-    expect( plsql.employees_api.get_empolyee(1) ).to eq(expected)
+    expected = plsql.employees.first('where employee_id = :id', 100)
+    expect( plsql.employees_api.get_employee(100) ).to eq(expected)
   end
 
 end
