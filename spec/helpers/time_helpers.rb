adef9aece2c25c6ada5694b1829778483532c1bc
Jacek Gębal <jgebal@gmail.com>
2015-12-21 00:32:08 +0000

    added more tests to start with

--- spec/helpers/time_helpers.rb
+++ spec/helpers/time_helpers.rb
@@ -3,3 +3,9 @@ def Time.today
   t = Time.now
   Time.local(t.year, t.month, t.day)
 end unless Time.respond_to?(:today)
+
+def duration(&block)
+  start = Time.now
+  yield if block_given?
+  Time.now - start
+end
